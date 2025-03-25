1c014aa5d919d57b05999472ddf38bb36c89c32e
Ido Berkovits <berkovitsido@gmail.com>
2025-03-25 11:16:41 +0200

    Changed Hello print statement again

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("This is a different print statement " + i);
+            System.out.println("Another printing statement " + i);
         }
     }
 }
