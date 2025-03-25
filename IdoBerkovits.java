c1c458b007a2087ee496e326a9cb27244e1aff71
Ido Berkovits <berkovitsido@gmail.com>
2025-03-25 11:18:13 +0200

    Changed IdoBerkovits print statement iterations

--- IdoBerkovits.java
+++ IdoBerkovits.java
@@ -1,6 +1,6 @@
 public class IdoBerkovits {
     public static void main(String[] args) {
-        for (int i = 1; i <= 8; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Printing from IdoBerkovits file" + i);
         }
     }
